e198d707e6c3e074ff9a8af21ffceb5a42b7d5d4
wkflanders <wflan@seas.upenn.edu>
2025-10-26 15:33:11 -0400

    fix: bunch of utils fixes

--- lua/anysphere/util.lua
+++ lua/anysphere/util.lua
@@ -1,11 +1,62 @@
 local M = {}
 
-function M.hi(group, spec)
-	vim.api.nvim_set_hl(0, group, spec or {})
+local function normalize(spec)
+	if not spec then
+		return {}
+	end
+	local out = {}
+	local allowed = {
+		fg = true,
+		bg = true,
+		sp = true,
+		blend = true,
+		nocombine = true,
+		bold = true,
+		italic = true,
+		underline = true,
+		undercurl = true,
+		underdouble = true,
+		underdotted = true,
+		underdashed = true,
+		strikethrough = true,
+		standout = true,
+		reverse = true,
+		link = true,
+		default = true,
+	}
+	for k, v in pairs(spec) do
+		if allowed[k] then
+			out[k] = v
+		end
+	end
+	local gui = spec.gui
+	if type(gui) == "string" and gui:lower() ~= "none" then
+		for attr in gui:gmatch("[^,%s]+") do
+			attr = attr:lower()
+			if attr == "bold" then
+				out.bold = true
+			elseif attr == "italic" then
+				out.italic = true
+			elseif attr == "underline" then
+				out.underline = true
+			elseif attr == "undercurl" then
+				out.undercurl = true
+			elseif attr == "strikethrough" or attr == "strike" then
+				out.strikethrough = true
+			elseif attr == "inverse" or attr == "reverse" then
+				out.reverse = true
+			end
+		end
+	end
+	return out
 end
 
-function M.link(a, b)
-	vim.api.nvim_set_hl(0, a, { link = b })
+function M.hi(group, spec)
+	vim.api.nvim_set_hl(0, group, normalize(spec))
+end
+
+function M.link(from, to)
+	vim.api.nvim_set_hl(0, from, { link = to, default = false })
 end
 
 return M
